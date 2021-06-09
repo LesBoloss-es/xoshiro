1e75b33391878edfbe13192b3685e2f75d4de6b4
Niols <niols@niols.fr>
2021-06-09 14:48:36 +0200

    prepare for state vs. stateless benchmarks

--- bench/run.ml
+++ bench/run.ml
@@ -1,22 +1,25 @@
-module type GEN = MakeRandom.Sig.BASIC
+module type BASIC = MakeRandom.Sig.BASIC
+module type FULL = MakeRandom.Sig.FULL
 
-let generators : (string * (module GEN)) list = [
-  "stdlib",                  (module Random);
-  "splitmix64 (pure)",       (module Splitmix64_pure);
-  "xoshiro256++ (pure)",     (module Xoshiro256plusplus_pure);
-  "xoshiro256++ (bindings)", (module Xoshiro256plusplus_bindings);
+type gen = B of (module BASIC) | F of (module FULL)
+
+let gens : (string * gen) list = [
+  "stdlib",                  F (module Random);
+  "splitmix64 (pure)",       F (module Splitmix64_pure);
+  "xoshiro256++ (pure)",     F (module Xoshiro256plusplus_pure);
+  "xoshiro256++ (bindings)", F (module Xoshiro256plusplus_bindings);
 ]
 
-type gen_to_test = GTT : ((module GEN) -> unit -> 'a) -> gen_to_test
+type gen_to_test = GTT : ((module BASIC) -> unit -> 'a) -> gen_to_test
 
 let tests : (string * gen_to_test) list = [
-  "bits",      GTT (fun (module Gen : GEN) () -> Gen.bits ());
-  "int",       GTT (fun (module Gen : GEN) () -> Gen.int (1 lsl 30 - 1));
-  "int32",     GTT (fun (module Gen : GEN) () -> Gen.int32 Int32.max_int);
-  "int64",     GTT (fun (module Gen : GEN) () -> Gen.int64 Int64.max_int);
-  "nativeint", GTT (fun (module Gen : GEN) () -> Gen.nativeint Nativeint.max_int);
-  "float",     GTT (fun (module Gen : GEN) () -> Gen.float 1.);
-  "bool",      GTT (fun (module Gen : GEN) () -> Gen.bool ());
+  "bits",      GTT (fun (module Gen : BASIC) () -> Gen.bits ());
+  "int",       GTT (fun (module Gen : BASIC) () -> Gen.int (1 lsl 30 - 1));
+  "int32",     GTT (fun (module Gen : BASIC) () -> Gen.int32 Int32.max_int);
+  "int64",     GTT (fun (module Gen : BASIC) () -> Gen.int64 Int64.max_int);
+  "nativeint", GTT (fun (module Gen : BASIC) () -> Gen.nativeint Nativeint.max_int);
+  "float",     GTT (fun (module Gen : BASIC) () -> Gen.float 1.);
+  "bool",      GTT (fun (module Gen : BASIC) () -> Gen.bool ());
 ]
 
 let print_header test_name =
@@ -28,10 +31,14 @@ let print_header test_name =
 
 let run_test gen_to_test =
   Core.Command.run @@ Core_bench.Bench.make_command (
-    List.map
-      (fun (name, (module Gen : GEN)) ->
-         Core_bench.Bench.Test.create ~name (gen_to_test (module Gen : GEN)))
-      generators
+    List.concat_map
+      (fun (name, gen) ->
+         match gen with
+         | B (module G : BASIC) ->
+           [ Core_bench.Bench.Test.create ~name (gen_to_test (module G : BASIC)) ]
+         | F (module G : FULL) ->
+           [ Core_bench.Bench.Test.create ~name (gen_to_test (module G : BASIC)) ])
+      gens
   )
 
 let () =
