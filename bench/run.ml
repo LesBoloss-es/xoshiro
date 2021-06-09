70c9af8e6035782efd9a4a86c36cf3870b283f58
Niols <niols@niols.fr>
2021-06-09 15:03:10 +0200

    bench also state vs. default state

--- bench/run.ml
+++ bench/run.ml
@@ -10,16 +10,37 @@ let gens : (string * gen) list = [
   "xoshiro256++ (bindings)", F (module Xoshiro256plusplus_bindings);
 ]
 
-type gen_to_test = GTT : ((module BASIC) -> unit -> 'a) -> gen_to_test
+type basic_gen_to_test = BGTT : ((module BASIC) -> unit -> 'a) -> basic_gen_to_test
+type  full_gen_to_test = FGTT : ((module FULL) -> unit -> 'a) ->  full_gen_to_test
 
-let tests : (string * gen_to_test) list = [
-  "bits",      GTT (fun (module Gen : BASIC) () -> Gen.bits ());
-  "int",       GTT (fun (module Gen : BASIC) () -> Gen.int (1 lsl 30 - 1));
-  "int32",     GTT (fun (module Gen : BASIC) () -> Gen.int32 Int32.max_int);
-  "int64",     GTT (fun (module Gen : BASIC) () -> Gen.int64 Int64.max_int);
-  "nativeint", GTT (fun (module Gen : BASIC) () -> Gen.nativeint Nativeint.max_int);
-  "float",     GTT (fun (module Gen : BASIC) () -> Gen.float 1.);
-  "bool",      GTT (fun (module Gen : BASIC) () -> Gen.bool ());
+let tests : (string * basic_gen_to_test * full_gen_to_test) list = [
+  "bits",
+  BGTT (fun (module G : BASIC) () -> G.bits ()),
+  FGTT (fun (module G : FULL) -> let state = G.get_state () in fun () -> G.State.bits state);
+
+  "int",
+  BGTT (fun (module G : BASIC) () -> G.int (1 lsl 30 - 1)),
+  FGTT (fun (module G : FULL) -> let state = G.get_state () in fun () -> G.State.int state);
+
+  "int32",
+  BGTT (fun (module G : BASIC) () -> G.int32 Int32.max_int),
+  FGTT (fun (module G : FULL) -> let state = G.get_state () in fun () -> G.State.int32 state);
+
+  "int64",
+  BGTT (fun (module G : BASIC) () -> G.int64 Int64.max_int),
+  FGTT (fun (module G : FULL) -> let state = G.get_state () in fun () -> G.State.int64 state);
+
+  "nativeint",
+  BGTT (fun (module G : BASIC) () -> G.nativeint Nativeint.max_int),
+  FGTT (fun (module G : FULL) -> let state = G.get_state () in fun () -> G.State.nativeint state);
+
+  "float",
+  BGTT (fun (module G : BASIC) () -> G.float 1.),
+  FGTT (fun (module G : FULL) -> let state = G.get_state () in fun () -> G.State.float state);
+
+  "bool",
+  BGTT (fun (module G : BASIC) () -> G.bool ()),
+  FGTT (fun (module G : FULL) -> let state = G.get_state () in fun () -> G.State.bool state);
 ]
 
 let print_header test_name =
@@ -29,21 +50,22 @@ let print_header test_name =
   Format.printf "\n## %s [ %s ] %s ##@."
     (String.make left '=') test_name (String.make right '=')
 
-let run_test gen_to_test =
+let run_test basic_gen_to_test full_gen_to_test =
   Core.Command.run @@ Core_bench.Bench.make_command (
     List.concat_map
       (fun (name, gen) ->
          match gen with
          | B (module G : BASIC) ->
-           [ Core_bench.Bench.Test.create ~name (gen_to_test (module G : BASIC)) ]
+           [ Core_bench.Bench.Test.create ~name (basic_gen_to_test (module G : BASIC)) ]
          | F (module G : FULL) ->
-           [ Core_bench.Bench.Test.create ~name (gen_to_test (module G : BASIC)) ])
+           [ Core_bench.Bench.Test.create ~name (basic_gen_to_test (module G : BASIC)) ;
+             Core_bench.Bench.Test.create ~name:(name ^ " [state]") (full_gen_to_test (module G : FULL)) ])
       gens
   )
 
 let () =
   List.iter
-    (fun (test_name, GTT gen_to_test) ->
+    (fun (test_name, BGTT basic_gen_to_test, FGTT full_gen_to_test) ->
        print_header test_name;
-       run_test gen_to_test)
+       run_test basic_gen_to_test full_gen_to_test)
     tests
