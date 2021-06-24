b2dac7dd1225fdf9784c609166fa93306cd83b98
Niols <niols@niols.fr>
2021-06-24 18:35:37 +0200

    extend CI to all OCaml 4.* and macOS

--- xoshiro256plusplus/src/pure/xoshiro256plusplus_pure.ml
+++ xoshiro256plusplus/src/pure/xoshiro256plusplus_pure.ml
@@ -1,33 +1,30 @@
-let[@inline] rotl x k =
+let rotl x k =
   let open Int64 in
   logor (shift_left x k) (shift_right_logical x (64 - k))
-
-let ( .!() ) = Array.unsafe_get
-let ( .!()<- ) = Array.unsafe_set
 
 let next s =
   let open Int64 in
 
   (* const uint64_t result = rotl(s[0] + s[3], 23) + s[0]; *)
-  let result = Int64.add (rotl (Int64.add s.!(0) s.!(3)) 23) s.!(0) in
+  let result = Int64.add (rotl (Int64.add s.(0) s.(3)) 23) s.(0) in
 
   (* const uint64_t t = s[1] << 17; *)
-  let t = shift_left s.!(1) 17 in
+  let t = shift_left s.(1) 17 in
 
   (* s[2] ^= s[0]; *)
-  s.!(2) <- logxor s.!(2) s.!(0);
+  s.(2) <- logxor s.(2) s.(0);
   (* s[3] ^= s[1]; *)
-  s.!(3) <- logxor s.!(3) s.!(1);
+  s.(3) <- logxor s.(3) s.(1);
   (* s[1] ^= s[2]; *)
-  s.!(1) <- logxor s.!(1) s.!(2);
+  s.(1) <- logxor s.(1) s.(2);
   (* s[0] ^= s[3]; *)
-  s.!(0) <- logxor s.!(0) s.!(3);
+  s.(0) <- logxor s.(0) s.(3);
 
   (* s[2] ^= t; *)
-  s.!(2) <- logxor s.!(2) t;
+  s.(2) <- logxor s.(2) t;
 
   (* s[3] = rotl(s[3], 45); *)
-  s.!(3) <- rotl s.!(3) 45;
+  s.(3) <- rotl s.(3) 45;
 
   (* return result; *)
   result
