4763a92c3a3886bf08e14baaccdfb62f123141cd
Niols <niols@niols.fr>
2021-06-24 13:30:50 +0200

    note on the bindings attempt

--- xoshiro256plusplus/src/bindings/xoshiro256plusplus_bindings.ml
+++ xoshiro256plusplus/src/bindings/xoshiro256plusplus_bindings.ml
@@ -10,3 +10,11 @@ include MakeRandom.Full30Init64(struct
 
     let default_seed = 135801055
   end)
+
+(* Note to self [from Niols]: One can see that the basic functions (eg. [val
+   bits: unit -> int]) in fact hide an OCaml default state which will be passed
+   to C every time, which means that, in order to run a [unit -> int], we have
+   to copy and unbox a state. It is then tempting to duplicate the code, to
+   write C function using a C state, and to bind these functions to OCaml. I
+   have tried, and it really increases the complexity of the code for...
+   nothing. like at all. like it is even possibly slower than before. *)
